d8e9a093d50cfff56f3eb443f0883033c047570f
aldiyar.gibadatov@gmail.com <student@ALEM-F4-R03-04.alem.school>
2023-03-08 00:40:51 +0600

    add flag and commet

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
-	"os"
 
 	"project/internal/handler"
 )
 
 var (
-	host     = "localhost"
 	connType = "tcp"
-	port     = "8989"
+
+	host string
+	port string
 )
 
 func main() {
-	if len(os.Args) == 2 {
-		port = os.Args[1]
-	} else if len(os.Args) >= 2 {
-		fmt.Printf("[USAGE]: ./TCPChat $port\n")
-		return
-	}
+	flag.StringVar(&port, "port", "8989", "Port on which to run")
+	flag.StringVar(&host, "host", "localhost", "The address where it will be launched")
+	flag.Parse()
+
 	listen, err := net.Listen(connType, host+":"+port)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Printf("Listening on the port :%v\n", port)
 
-	defer func() {
-		log.Printf("End listen tcp port: %s\n", port)
-		listen.Close()
-	}()
+	fmt.Printf("Listening on the port: %v\n", port)
+
+	// defer func() {
+	// 	log.Printf("End listen tcp port: %s\n", port)
+	// 	listen.Close()
+	// }()
 
 	if err = handler.Run(listen); err != nil {
 		log.Fatalln(err)
